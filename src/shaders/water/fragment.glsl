f5dbb1cfd22dd8e17d4b254d415258341499c8bd
Esteban Acuna <esteban.acuna@sweetrush.com>
2024-10-07 11:59:38 -0600

    transparency tweak

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -9,5 +9,5 @@ void main() {
     #include <colorspace_fragment>
     vec3 color = mix(uDepthColor, uSurfaceColor, (vElevation + uColorOffset) * uColorMultiplier);
 
-    gl_FragColor = vec4(color, 0.6);
+    gl_FragColor = vec4(color, 0.8);
 }
